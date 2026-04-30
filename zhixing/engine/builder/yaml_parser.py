18348bed222e88e9ab43fcbc2d5bd9989c7f2da1
zjz2891zjz <zjz165604@outlook.com>
2026-04-30 11:32:13 +0800

    YamlParser add _load_yaml and _load_secrets

--- zhixing/engine/builder/yaml_parser.py
+++ zhixing/engine/builder/yaml_parser.py
@@ -1,17 +1,15 @@
 import os
 import yaml
+import logging
 from typing import Dict
 
 from zhixing.core.agent.interfaces import BaseAgent
 from zhixing.devices.base import BaseDevice
 
-
-
-
-
+logger = logging.getLogger(__name__)
 
 class YamlParser:
-    def __init__(self, config_path: str, secrets_path: str = "secrets.yaml") -> None:
+    def __init__(self, config_path: str, secret_path: str = "secrets.yaml") -> None:
         """YamlParser: Be responsible for parsing YAML and pulling up the Agent
 
         Args:
@@ -28,3 +26,20 @@ class YamlParser:
         device = None
         agent = None
         return device, agent
+
+    @staticmethod
+    def _load_yaml(path: str) -> Dict:
+        if not os.path.exists(path):
+            raise FileNotFoundError(f"Config file not found: {path}")
+        with open(path, 'r', encoding='utf-8') as f:
+            return yaml.safe_load(f)
+    
+    @staticmethod
+    def _load_secrets(secret_path):
+        
+        if os.path.exists(secret_path):
+            logger.info(f"🔑 Found secrets file: {secret_path}")
+            return YamlParser._load_yaml(secret_path)
+        else:
+            logger.warning("⚠️ No secrets.yaml found in configs/. placeholders like ${KEY} may fail.")
+            return {}
