6019050729dd6bb26b1af13767e2479943c9769e
zjz2891zjz <zjz165604@outlook.com>
2026-05-04 20:29:57 +0800

    Modify logger

--- zhixing/utils/utils.py
+++ zhixing/utils/utils.py
@@ -50,7 +50,7 @@ def setup_logging(log_file="app.log", log_level=logging.INFO):
     # formatter = logging.Formatter("%(message)s - %(asctime)s - %(name)s - %(levelname)s")
     
     formatter = logging.Formatter(
-        fmt="%(asctime)s [%(levelname)-7s] [%(name)s] ➜ %(message)s",
+        fmt="%(asctime)s [%(levelname)-5s] %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S"
     )
 
